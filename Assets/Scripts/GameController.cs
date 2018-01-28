c42d095af0bbd83a8620077fd4b8e943530c494a
TasiYokan <guanghao.yang@outlook.com>
2018-01-28 10:36:34 -0500

    Add audio module

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TasiYokan.Audio;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        new SingleAudio("BGM_LOOP", AudioLayer.Bgm).Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
