fe9c58294797fb5a2295e443d00c4206b854d9fd
Renee Mei <meiqianye@gmail.com>
2018-01-27 22:35:11 -0500

    add ending

---
+++ Assets/PlaySeedAnimation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySeedAnimation : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        SeedFlying();
+
+    }
+
+    // Update is called once per frame
+   void  SeedFlying()
+    {
+        this.GetComponent<Animator>().Play("idle");
+    }
+}
